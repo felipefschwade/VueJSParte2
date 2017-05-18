6709dbf0ee717767184b21f3c493ecf46c2b13be
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-05-18 00:38:19 -0400

    Utilizando a propriedade binding para receber objetos JS dentro dos parametros de nossa diretiva.

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -5,8 +5,16 @@ Vue.directive('meu-transform', {
     bind(el, binding, vnode) {
         let current = 0;
         el.addEventListener('dblclick', function() {
-            current+=90;
+            let incremento = 90;
+            let animacao = true;
+
+            if(binding.value) {
+                animacao = binding.value.animate;
+                incremento = binding.value.incremento;
+            }
+            current+=incremento;
             this.style.transform = `rotate(${current}deg)`;
+            if (animacao) this.style.transition = "transform 0.5s" ;
         });
     }
 });
